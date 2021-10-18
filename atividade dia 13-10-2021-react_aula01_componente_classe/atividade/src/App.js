1a60fa0abcc168ba70ed846100985fe06dc95316
Luis Henrique <LuisHenSilva@Outlook.com>
2021-10-18 12:20:44 -0300

    react classe

---
+++ atividade dia 13-10-2021-react_aula01_componente_classe/atividade/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Componente from './classe';
+
+ function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+     <h1>Olá mundo React!</h1>
+   <Componente/>
+        </p>
+
+      </header>
+    </div>
+  );
+}
+export default App;
